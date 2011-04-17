3cdf718113cd455bd8689aa45f41704c112d316e
Jérôme Wax <nospam@lo2k.net>
2011-04-17 18:05:34 +0200

    add some code

--- listing.py
+++ listing.py
@@ -1,6 +1,15 @@
+#!/usr/bin/python
 import os
+import sys
+import getopt
 
-
+class DirEntry(object):
+	def __init__(self, dirname, filename):
+		self.dirname = dirname
+		self.filename = filename
+	
+	def prettyPrint(self):
+		print self.dirname+ " " + self.filename	
 
 def listing(directory):
 	for dirname, dirnames, filenames in os.walk(directory):
@@ -15,5 +24,27 @@ def listing(directory):
 					
 		for filename in filenames:
 			if "avi" in filename:
-				print dirname+"/"+filename			
-listing("Disque dur maman")
+				dir = DirEntry(dirname, filename)
+				dir.prettyPrint()
+				
+
+def main():
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
+	except getopt.error, msg:
+		print msg
+		print "for help use --help"
+		sys.exit(2)
+	for o, a in opts:
+		if o in ("-h", "--help"):
+			print __doc__
+			sys.exit(0)
+	if len(args) !=1:
+		print "wrong argument size"
+	for arg in args:
+		listing(arg)
+
+#launch the program		
+if __name__ == "__main__":
+	main()
+		
